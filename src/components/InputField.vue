c0dc4087a7479ac4ea3a58791578e33dd0c76235
pgmgent-jarnvans <jarnvans@student.arteveldehs.be>
2021-05-25 17:39:09 +0200

    first commit

---
+++ src/components/InputField.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="form-group">
+  <input :id="id" :type="type" :placeholder="placeholder" @input="updateValue">
+</div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'InputField',
+  props: {
+    id: String,
+    placeholder: String,
+    type: String,
+  },
+  methods: {
+    updateValue(e: InputEvent) {
+      const element = e.target as HTMLInputElement;
+      const { value } = element;
+      this.$emit('updateValue', [
+        this.id,
+        value,
+      ]);
+      console.log(element.value);
+    },
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <p class="blue">{{ msg }}</p>
+    <form @submit="sendForm">
+      <InputField
+        type="text"
+        placeholder="Firstname"
+        id="firstname"
+        @updateValue="updateInputValue($event)"
+      />
+      <InputField type="text" placeholder="Lastname" id="lastname"/>
+      <InputField type="email" placeholder="Email" id="email"/>
+      <InputField type="password" placeholder="Password" id="password"/>
+      <input type="submit" />
+    </form>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import InputField from '@/components/InputField.vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
   name: 'Home',
+  data: () => ({
+    msg: 'Hello!',
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+  }),
   components: {
+    InputField,
     HelloWorld,
   },
+  methods: {
+    async updateInputValue(args: [string, string]) {
+      const [id, value] = args;
+      console.log(id);
+      console.log(value);
+    },
+    async sendForm() {
+      console.log('I send dude');
+    },
+  },
 });
+
 </script>
